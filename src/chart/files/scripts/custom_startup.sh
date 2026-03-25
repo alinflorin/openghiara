7d4e54fcd41179e339b5a300b711a9abfdbd0d87
Alin-Florin Ciu <alinflorinciu@icloud.com>
2026-03-25 11:00:09 +0200

    asd

--- src/chart/files/scripts/custom_startup.sh
+++ src/chart/files/scripts/custom_startup.sh
@@ -77,7 +77,7 @@ fi
 
 if [ ! -f /home/kasm-user/.markers/pyautogui-mcp ]; then
   echo "Installing pyautogui-mcp..."
-  pip install --python /home/kasm-user/Software/python/bin/python3 pyautogui-mcp
+  pip install pyautogui-mcp
   touch /home/kasm-user/.markers/pyautogui-mcp
   echo "pyautogui-mcp setup complete."
 fi
