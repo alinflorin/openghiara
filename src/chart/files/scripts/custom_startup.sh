008e74740975f6447b3715f77bbad45b42a184f9
Alin-Florin Ciu <alinflorinciu@icloud.com>
2026-03-25 11:45:56 +0200

    sdf

--- src/chart/files/scripts/custom_startup.sh
+++ src/chart/files/scripts/custom_startup.sh
@@ -77,7 +77,7 @@ fi
 
 if [ ! -f /home/kasm-user/.markers/pyautogui-mcp ]; then
   echo "Installing pyautogui-mcp..."
-  pip install pyautogui-mcp Pillow
+  pip install pyautogui-mcp Pillow pyscreenshot python-xlib
   touch /home/kasm-user/.markers/pyautogui-mcp
   echo "pyautogui-mcp setup complete."
 fi
